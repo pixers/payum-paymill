053042e1cd5466346b20c56ab39c0fcfc70e6d83
Jacek Smyda <jacek.smyda@netteam.pl>
2016-02-01 21:39:41 +0100

    Adding examples.

---
+++ Examples/guide_payment_end.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>PAYMILL QUICKSTART</title>
+
+    <link href="/Examples/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/Examples/css/styles.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">PAYMILL QUICKSTART</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Your payment was successfully processed.</h1>
+
+        <p class="lead">It's easy to integrate payments! You just did it!</p>
+        <p class="lead">Thanks for following this Quickstart!
+          <br>
+          Get further information on <a href="http://developers.paymill.com">the PAYMILL Developer Centre</a>
+        </p>
+
+<pre>
+<?php
+
+print_r($transaction);
+
+?>
+</pre>
+
+      </div>
+
+    </div>
+  </body>
+</html>
